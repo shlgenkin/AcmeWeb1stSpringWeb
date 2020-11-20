e2869c3fa48def9d00f6962dec58db550eedff42
Shlomo Genkin <shlgenkin@gmail.com>
2020-11-19 21:28:05 -0500

    Test Methods

--- src/test/java/com/acme/statusmgr/ServerStatusControllerTests.java
+++ src/test/java/com/acme/statusmgr/ServerStatusControllerTests.java
@@ -19,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -49,6 +50,63 @@ public class ServerStatusControllerTests {
         this.mockMvc.perform(get("/server/status").param("name", "RebYid"))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(jsonPath("$.contentHeader").value("Server Status requested by RebYid"));
+    }
+
+    @Test
+    public void withParamShouldReturnTailored_Name_Basic_Mem_Op_Ext() throws Exception {
+        this.mockMvc.perform(get("/server/status/detailed?details=operations"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, and is operating normally"));
+
+        this.mockMvc.perform(get("/server/status/detailed?details=memory,operations,extensions&name=yaakov"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.contentHeader").value("Server Status requested by yaakov"))
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, and its memory is running low, " +
+                        "and is operating normally, and is using these extensions - [Hypervisor, Kubernetes, RAID-6]"));
+
+        this.mockMvc.perform(get("/server/status/detailed?details=operations,memory&name=yaakov"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.contentHeader").value("Server Status requested by yaakov"))
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, and is operating normally, " +
+                        "and its memory is running low"));
+
+        this.mockMvc.perform(get("/server/status/detailed?name=yaakov&details=memory,operations"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.contentHeader").value("Server Status requested by yaakov"))
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, " +
+                        "and its memory is running low, and is operating normally"));
+
+        this.mockMvc.perform(get("/server/status/detailed?name=yaakov&details=memory,operations,memory"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.contentHeader").value("Server Status requested by yaakov"))
+                .andExpect(jsonPath("$.statusDesc").value("Server is up, " +
+                        "and its memory is running low, and is operating normally, and its memory is running low"));
+    }
+
+    @Test
+    public void noDetailsParamShouldReturnCustomException() throws Exception {
+        this.mockMvc.perform(get("/server/status/detailed"))
+                .andDo(print()).andExpect(status().isBadRequest()).andExpect(status().reason(is("Required List parameter 'details' is not present in request")));
+
+/*
+   A URL request that ends with 'details' or 'details=' is a tricky case.
+   If 'details' parameter requirement and error implemented by using a null check and
+   generating a custom message, the following test would fail. In some implementations no error would be
+   generated and in others the default message would be returned instead of the custom one.
+   Checking for the details list via .isEmpty() as this implementation does solves this issue.
+ */
+        this.mockMvc.perform(get("/server/status/detailed?details="))
+                .andDo(print()).andExpect(status().isBadRequest()).andExpect(status().reason(is("Required List parameter 'details' is not present in request")));
+    }
+
+    @Test
+    public void withParamShouldReturnCustomExceptionInvalidDetails() throws Exception {
+        this.mockMvc.perform(get("/server/status/detailed?details=memory,operations,RanDom"))
+                .andDo(print()).andExpect(status().isBadRequest()).andExpect(status().reason(is("Invalid details option: RanDom")));
+
+        this.mockMvc.perform(get("/server/status/detailed?details=memory,operations,RanDom&name=yaakov"))
+                .andDo(print()).andExpect(status().isBadRequest()).andExpect(status().reason(is("Invalid details option: RanDom")));
+
     }
 
 }
